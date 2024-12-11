9dadf86622c415b9043fdb3ae90fa8df427a0610
Zan_ <146116869+Run8unny@users.noreply.github.com>
2024-12-11 23:43:28 +0100

    icons added

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
 	return (
 		<div className='w-full h-full felx flex-col justify-center items-center'>
 			<h1 className='text-4xl'>hello</h1>
-			<p className='xs'>hello</p>
+			<p className='xs'>YOU</p>
 		</div>
 	);
 }
