21af680735242551539593d9c1e55d76624f2db0
Zan_ <146116869+Run8unny@users.noreply.github.com>
2024-12-17 00:38:50 +0100

    bug deleted

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
 				sm: ['1.618rem', { lineHeight: '2.427rem' }],
 				base: ['2.617rem', { lineHeight: '3.925rem' }],
 				lg: ['4.235rem', { lineHeight: '6.325rem' }],
-				xl: '11.089rem', 
+				xl: '11.089rem',
 				'2xl': '17.942rem',
 				'3xl': '29.030rem',
 				'4xl': '46.970rem',
@@ -38,15 +38,3 @@ module.exports = {
 	},
 	plugins: [],
 };
-
-
-fontSize: {
-	xs: ['1rem', { lineHeight: '1rem' }],
-	sm: ['1.618rem', { lineHeight: '1.25rem' }],
-	base: ['2.617rem', { lineHeight: '1.5rem' }],
-	lg: ['4.235rem', { lineHeight: '1.75rem' }],
-	xl: ['11.089rem', { lineHeight: '1.75rem' }],
-	'2xl': ['17.942rem', { lineHeight: '2rem' }],
-	'3xl': ['29.030rem', { lineHeight: '2.25rem' }],
-	'4xl': ['46.970rem', { lineHeight: '2.5rem' }],
-  },
